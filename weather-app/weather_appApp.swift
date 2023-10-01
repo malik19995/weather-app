af96cbf75ac4de39d6c6a63160d7d72fbe06af8e
Hamza A.Malik <malik1991995@gmail.com>
2023-10-01 16:00:14 +0500

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,32 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Hamza Ahmad on 01/10/2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct weather_appApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
